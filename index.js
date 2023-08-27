d58971744a056035c4c6de03e1106d28e81d8b11
SupraSensum <107372360+SupraSensum@users.noreply.github.com>
2023-08-27 01:41:01 +0000

    Create fragile resizing function

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-let operandA,
-   operandB,
-   operator;
-
+const theCalculator = document.getElementById('theCalculator');
+const contentContainer = document.getElementById('content');
 const calc = {
    '+': (a, b) => a + b,
    '-': (a, b) => a - b,
@@ -9,8 +7,33 @@ const calc = {
    '/': (a, b) => a / b,
 }
 
+let operandA,
+   operandB,
+   operator;
+
+setCalcHeightToPropOfWidth();
+window.addEventListener('resize', setCalcHeightToPropOfWidth);
+
 function operate(op, a, b) {
    return calc[op](a, b);
+}
+
+// set calculator height to be a proportion of its width
+function setCalcHeightToPropOfWidth() {
+   const calcWidth = theCalculator.clientWidth;
+   const contentHeight = contentContainer.clientHeight;
+   const MAX_HEIGHT = contentHeight * 0.75;
+   const MIN_HEIGHT = 350;
+
+   let newHeight = calcWidth * 1.5
+
+   if (newHeight > MAX_HEIGHT){
+      newHeight = MAX_HEIGHT;
+   } else if (newHeight < MIN_HEIGHT) {
+      newHeight = MIN_HEIGHT;
+   }
+
+   theCalculator.style.height = `${newHeight}px`;
 }
 
 // debugging
